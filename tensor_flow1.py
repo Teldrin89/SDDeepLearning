adce034bfaab24fc68ea59f72f96692b6f6bd00f
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-01-10 23:52:31 +0100

    running the initial deep learning assignment: - from the keras dataset use mnist (images of numbers 0-9)

--- tensor_flow1.py
+++ tensor_flow1.py
@@ -1,4 +1,9 @@
 # introduction to deep learning - part1
+# need tensorflow version 1.10 or greater
 import tensorflow as tf
+# import matplotlib for visualisation
+
 
 print(tf.__version__)
+
+mnist = tf.keras.datasets.mnist  # 28x28 images of hand written digits 0-9
