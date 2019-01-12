3a60a46c2505867842491ecf65093e083f4cae4f
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-01-12 20:08:56 +0100

    added the warning disable for AVX computation

--- tf1_chek_of_saved_model.py
+++ tf1_chek_of_saved_model.py
@@ -3,6 +3,10 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+# To disable the the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 # use a template dataset from keras api - it's kind of "hello world" example for ML
 mnist = tf.keras.datasets.mnist  # 28x28 images of hand written digits 0-9
