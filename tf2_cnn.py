c074e5b2c9cb0c7178ef8029872642dbfe8ed804
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-01-15 00:23:58 +0100

    load datasets and normalize input

--- tf2_cnn.py
+++ tf2_cnn.py
@@ -1,8 +1,17 @@
 # second file for part2 of tensor flow deep learning tutorial - in part1 there are instructions to build datasets
 # for the input and output of training data of pictures of cats and dogs
 # this file will contain the model and scripts for machine learning of created model
-# import required libraries
+# import required libraries: tensorflow
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import os
-import cv2
+# import from keras the model
+from tensorflow.keras.models import Sequential
+# import from keras the layers used - Dense and Flatten (for last layer)
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+# import pickle to load saved datasets
+import pickle
+# open input (X) and output (y) train datasets
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+# the next step is to normalize data - scale it - as the image data has max value of 255 each will be divide by max
+X = X/255
+
