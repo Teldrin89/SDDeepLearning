a045f74b3a59364c7de9767b7af45c188851285a
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-01-15 00:32:15 +0100

    created a sequential model and added 1st layer (conv.,activ., max-pool.)

--- tf2_cnn.py
+++ tf2_cnn.py
@@ -14,4 +14,13 @@ X = pickle.load(open("X.pickle", "rb"))
 y = pickle.load(open("y.pickle", "rb"))
 # the next step is to normalize data - scale it - as the image data has max value of 255 each will be divide by max
 X = X/255
-
+# start building a model - sequential model
+model = Sequential()
+# add 1st layer - convolutional 2D layer, with 64 as filter, 3x3 window (snap) and input shape taken from X but
+# without first (0) value as it stores the number of features instead of feature that we want
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+# still within 1st layer add activation layer (could be activation or pooling but in this example it will be
+# activation -> pooling)
+model.add(Activation("relu"))
+# last part for 1st layer is max-pooling layer with pool size of 2x2
+model.add(MaxPooling2D(pool_size=(2,2)))
