b431b23f118cf807c737f12ed8eba9b399060f45
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-01-14 23:27:32 +0100

    initial commit for second file of part 2 tensor flow tutorial

---
+++ tf2_cnn.py
@@ -0,0 +1,8 @@
+# second file for part2 of tensor flow deep learning tutorial - in part1 there are instructions to build datasets
+# for the input and output of training data of pictures of cats and dogs
+# this file will contain the model and scripts for machine learning of created model
+# import required libraries
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import cv2
